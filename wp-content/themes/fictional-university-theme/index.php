d68cc61357d59ee7503ebdc82a56e6bd9a820afc
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-31 07:36:48 -0500

    a

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,14 @@
-A disaster waiting to happen
+<?php
+
+while(have_posts()){
+    the_post(); ?>
+    <h2><a href ="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+    
+<?php
+        }
+
+?>
+
+
