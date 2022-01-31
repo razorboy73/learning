67f38b4ec65b73692b41ad32857f07984b3f146c
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-31 10:49:07 -0500

    js in footer

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -4,7 +4,7 @@ function university_files(){
     wp_enqueue_style("university_main_styles", get_template_directory_uri().'/build/style-index.css');
     wp_enqueue_style("university_extra_styles", get_template_directory_uri().'/build/index.css');
     wp_enqueue_style("font-awesome", get_template_directory_uri().'/fontawesome/css/all.min.css');
-    wp_enqueue_script("font-awesome-js", get_template_directory_uri().'/fontawesome/js/all.min.js');
+    wp_enqueue_script("font-awesome-js", get_template_directory_uri().'/fontawesome/js/all.min.js',array(), false, true);
 }
 
 
