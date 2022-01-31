07f13c8f34583576667ec586df4b38d0638daf87
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-01-31 11:11:36 -0500

    fonts

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files(){
+    wp_enqueue_style("custom-google-fonts", "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("university_main_styles", get_template_directory_uri().'/build/style-index.css');
     wp_enqueue_style("university_extra_styles", get_template_directory_uri().'/build/index.css');
     wp_enqueue_style("font-awesome", get_template_directory_uri().'/fontawesome/css/all.min.css');
