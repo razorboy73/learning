ccadc554672ec5efba588b6be9df1241e4e92321
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-02-03 13:33:53 -0500

    archives

---
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -0,0 +1,77 @@
+<?php
+get_header()
+?>
+   <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+            <?php
+            if (is_category( )){
+                 single_cat_title();
+            ?>
+            </h1>
+            <div class="page-banner__intro">
+            <p><?php echo category_description();?> </p>
+            </div>
+
+            <?php }
+
+            elseif(is_author()){
+            echo "Posts by ";   the_author();
+
+            ?>
+            </h1>
+            <div class="page-banner__intro">
+            <p><?php echo get_the_author_meta("description");?> </p>
+            </div>
+
+          <?php  }
+
+          else{
+              the_archive_title(); ?>
+
+            <div class="page-banner__intro">
+            <p><?php the_archive_description();?> </p>
+            </div>
+
+              
+          <?php }
+
+
+            ?>
+
+
+           
+        </div>
+        </div>
+  
+    <div class="container container--narrow page-section">
+   <?php
+    if(have_posts()){
+      while(have_posts()){ 
+        the_post()?>
+
+      <div class="post-item">
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+        <div class="metabox">
+          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y'); ?> in <?php the_category( ', ' ); ?></p>
+        </div>
+        <div class="generic-content">
+          <?php the_excerpt(); ?>
+          <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Read More</a></p>
+        </div>
+      
+      
+      
+      
+      </div>
+       
+     <?php }// end of while loop
+    echo paginate_links();
+    }// end of if stateent
+   ?>
+    </div>
+      
+<?php
+get_footer()
+?>
