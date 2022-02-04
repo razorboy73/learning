497a045c271dd0cbffeaba06694534434216b015
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-02-04 14:03:13 -0500

    muplugins

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,29 @@
+<?php
+
+
+function university_post_types(){
+    register_post_type("event", array(
+        //for the archive, it uses the register post type name parameter, not the label
+        //or url rewrite
+        "has_archive" => true,
+        "description" => "What is going on around campus",
+        "public" => true,
+        "labels" => array(
+            "name" => "Events",
+            "add_new_item" => "Add New Event",
+            "edit_item" => "Edit Your Silly Event",
+            "all_items" => "All Events",
+            "singular_name" => "Event",
+        ),
+        "menu_icon" => "dashicons-calendar",
+        'rewrite'     => array( 'slug' => 'amazing-events' ),
+        
+
+    ));
+
+}
+
+add_action("init","university_post_types");
+
+
+?>
