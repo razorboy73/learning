ccca11a8afd36c8ecda76c39624a2440a2c524cd
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-02-07 20:57:24 -0500

    edits

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -52,55 +52,64 @@ get_header(); ?>
               )
             )
           ));
-          while($homePageEvents->have_posts()){
-            $homePageEvents->the_post(); ?>
-         
 
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php the_permalink()?>">
-            <span class="event-summary__month"><?php 
+          if($homePageEvents->have_posts()){
+
+            echo "<hr class='section-break'>";
+            echo "<h2 class='headline headline--medium'> Upcoming ". get_the_title()." Events</h2>";
             
-            $eventDate = new DateTime(get_field('event_date'));
+            while($homePageEvents->have_posts()){
+                $homePageEvents->the_post(); ?>
             
-            echo $eventDate->format("M");
-            
-            
-            
-            
-            ?></span>
-              <span class="event-summary__day">
-              <?php 
-            
+
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink()?>">
+                <span class="event-summary__month"><?php 
+                
                 $eventDate = new DateTime(get_field('event_date'));
+                
+                echo $eventDate->format("M");
+                
+                
+                
+                
+                ?></span>
+                <span class="event-summary__day">
+                <?php 
+                
+                    $eventDate = new DateTime(get_field('event_date'));
+                
+                    echo $eventDate->format("j");
+                
+                
+                
+                
+                ?>
+                </span>
+                </a>
+                <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h5>
+                <p><?php 
+                if(has_excerpt()){
+                    echo get_the_excerpt();
+                }else{
+                    echo(wp_trim_words(get_the_content(),18));
+                }
+                
+                ?>  <a href="<?php the_permalink()?>" class="nu gray">Read more</a></p>
+                </div> 
+            </div>
             
-                echo $eventDate->format("j");
-            
-            
-            
-            
+            <?php }
             ?>
-              </span>
-            </a>
-            <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h5>
-            <p><?php 
-              if(has_excerpt()){
-                echo get_the_excerpt();
-              }else{
-                echo(wp_trim_words(get_the_content(),18));
-              }
-              
-              ?>  <a href="<?php the_permalink()?>" class="nu gray">Read more</a></p>
-              </div> 
-          </div>
           
-          <?php }
-           wp_reset_postdata()
-          ?>
+
+            
     </div> 
 
 
-    
+    <?php
+        }?>
    
     
 <?php
