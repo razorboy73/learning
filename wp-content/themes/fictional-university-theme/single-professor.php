daa851ab0ec482fff1e643e19fb13fa2adcf508f
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-02-08 23:03:07 -0500

    pageBanner code

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -2,17 +2,13 @@
 get_header(); ?>
 
 <?php while(have_posts()){
-    the_post(); ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage =  get_field('page_banner_background_image');
-        echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-         
-            <h1 class="page-banner__title"><?php the_title() ?></h1>
-            <div class="page-banner__intro">
-            <p><?php the_field("page_banner_subtitle")?></p>
-            </div>
-        </div>
+    the_post(); 
+    pageBanner(array(
+        "title" => "hello, this is the title",
+        "subtitle" => "this is the sub title",
+        "photo" =>"https://images.unsplash.com/photo-1644333192098-75573dacbb0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+
+    ));?>
     </div>
     <div class="container container--narrow page-section">
  
