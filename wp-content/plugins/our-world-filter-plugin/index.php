c15aae13c4d62d19c4dc6b2ece9dc5ace9e5acad
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-02-17 11:35:25 -0500

    plugin css

--- wp-content/plugins/our-world-filter-plugin/index.php
+++ wp-content/plugins/our-world-filter-plugin/index.php
@@ -21,11 +21,21 @@ class OurWordFilterPlugin {
     }
 
     function ourMenu(){
-        add_menu_page("Words To Filter","Word Filter","manage_options","ourwordfilter", array(
+        // add_menu_page("Words To Filter","Word Filter","manage_options","ourwordfilter", array(
+        //     $this,
+        //     "wordFilterPage"
+        //      ),
+        //      plugin_dir_url(__FILE__).'custom.svg',100);
+        //20 x20 is a good icon height
+       
+        
+        
+        $mainPageHook = add_menu_page("Words To Filter","Word Filter","manage_options","ourwordfilter", array(
             $this,
             "wordFilterPage"
              ),
-             "dashicons-smiley",100);
+             "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==",100);
+       
         add_submenu_page( "ourwordfilter", "Words To Filter", "Words List", "manage_options", "ourwordfilter", array(
                 $this,
                 "wordFilterPage"
@@ -35,10 +45,31 @@ class OurWordFilterPlugin {
             $this,
             "optionsSubPage"
         ));
+
+        add_action("load-{$mainPageHook}", array(
+            $this, "mainPageAssets"
+        ));
+    }
+
+    function mainPageAssets(){
+
+        wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__). 'styles.css');
+
     }
     function wordFilterPage(){?>
 
-hi
+        <div class="wrap">
+            <h1>World Filter</h1>
+
+            <form action="" method="post">
+                <label for="plugin_words_to_filter"><p>Enter a <strong>comma separated</strong> list of words to filter from your site's contents</p></label>
+                    <div class="word-filter__flex-container">
+                        <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="Bad, mean, awful, horrible"></textarea>
+                    </div>
+                    <input type="submit" name="submit" id="submit"  class="button button-primary" value="Save Changes">
+            
+            </form>
+        </div>
     <?php }
 
     function optionsSubPage(){?>
