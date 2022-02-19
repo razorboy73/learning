618da2ef0c13636188c5e7328e0ceb6caa57587a
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-02-19 10:30:28 -0500

    jsx

--- wp-content/plugins/are-you-paying-attention/index.php
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -30,7 +30,7 @@ class AreYouPayingAttention{
     
     function adminAssets(){
 
-        wp_enqueue_script("ournewblocktype", plugin_dir_url(__FILE__)."test.js", array(
+        wp_enqueue_script("ournewblocktype", plugin_dir_url(__FILE__)."build/index.js", array(
             "wp-blocks",
             "wp-element"
         ));
